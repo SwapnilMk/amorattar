88bc9ee47e5ae5d659232a822da8398317341d7b
SwapnilMk <mswapnil218@gmail.com>
2024-02-24 00:43:27 +0530

    product add form basic functionality added!

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
-import ProductList from "./components/ProductList"
+import ProductList from "./components/ProductList";
 import AddProduct from "./components/AddProduct";
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 
-
-const App =()=>{
-  return(
+const App = () => {
+  return (
     <>
-    <Navbar />   
-     <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/product" element={<ProductList />}/>
-      <Route path="/addproduct" element={<AddProduct />}/>
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<ProductList />} />
+        <Route path="/addproduct" element={<AddProduct />} />
+      </Routes>
     </>
-    
-  )
-}
+  );
+};
 
 export default App;
--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -1,10 +1,47 @@
+import React, { useState } from 'react'; // Import statement corrected
+import axios from 'axios'; // Import statement added
+
 const AddProduct = () => {
+    const [product, setProduct] = useState({});
+
+    const handleChange = (e) => {
+        setProduct(prevProduct => ({
+            ...prevProduct,
+            [e.target.name]: e.target.value
+        }));
+        console.log(product);
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await axios.post('http://localhost:8080/products', product);
+            console.log(res.data);
+        } catch (error) {
+            console.error('Error while submitting product:', error);
+        }
+    }
+
     return (
         <>
-            <form>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="id">id:</label>
+                <br />
+                <input type="number" onChange={handleChange} name="id" id="id" />
+                <br />
                 <label htmlFor="name">Name:</label>
                 <br />
-                <input type="text" id="name" />
+                <input type="text" onChange={handleChange} name="name" id="name" />
+                <br />
+                <label htmlFor="description">Description:</label>
+                <br />
+                <input type="text" onChange={handleChange} name="description" id="description" />
+                <br />
+                <label htmlFor="price">Price:</label>
+                <br />
+                <input type="number" onChange={handleChange} name="price" id="price" />
+                <br />
+                <button type="submit">Submit</button> {/* Added type attribute */}
             </form>
         </>
     );
